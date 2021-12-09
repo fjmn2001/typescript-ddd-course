39a00dbcee81729e9491e2246471651d0bd7eaea
🚀 Francisco Marcano <fjmn2001@gmail.com>
2021-12-09 18:18:53 -0400

    feat: adding schema validation in infrastructure layer. Ref sn

--- src/apps/mooc/backend/routes/courses.route.ts
+++ src/apps/mooc/backend/routes/courses.route.ts
@@ -1,8 +1,15 @@
 import {Request, Response, Router} from 'express';
 import container from '../dependency-injection';
 import {CoursePutController} from '../controllers/CoursePutController';
+import {body} from 'express-validator';
+import {validateReqSchema} from './index';
 
 export const register = (router: Router) => {
+  const reqSchema = [
+    body('name').exists().isString(),
+    body('duration').exists().isString()
+  ];
+
   const coursePutController: CoursePutController = container.get('Apps.mooc.controllers.CoursePutController');
-  router.put('/courses/:id', (req: Request, res: Response) => coursePutController.run(req, res));
+  router.put('/courses/:id', reqSchema, validateReqSchema, (req: Request, res: Response) => coursePutController.run(req, res));
 };
